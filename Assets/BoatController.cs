ffec8b1cd6588526985293755b3499f576e405a6
田辺祐基 <dqalg253@ybb.ne.jp>
2017-10-11 18:51:38 +0900

    オリジナルゲーム作製　PlayRoom
    反射後の進行方向設定

---
+++ Assets/BoatController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoatController : MonoBehaviour {
+
+    private float speed = 3.5f;
+
+    Vector3 prev;
+
+    private float timeElapsed;
+
+    // Use this for initialization
+    void Start () {
+
+        this.GetComponent<Rigidbody>().AddForce(
+            (transform.forward ) * speed,
+             ForceMode.VelocityChange);
+
+        timeElapsed = 0.0f;        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeElapsed += Time.deltaTime;
+        StartCoroutine("Direction");
+    }
+    IEnumerator Direction()
+    {
+        Vector3 diff = transform.position - prev;
+        yield return new WaitForSeconds(0.1f);
+        transform.rotation = Quaternion.LookRotation(diff);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Wall")
+        {
+           
+            prev = transform.position;
+            
+        }
+    }
+}
